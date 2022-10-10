9024013b3ea40721fc024886bf547e05777b7005
amelieberry <aac.berry@gmail.com>
2022-10-10 11:01:02 +0200

    moved documentation.html to public

---
+++ index.js
@@ -0,0 +1,4 @@
+// imports the express module locally
+const express = require('express');
+// declares a variable that encapsulates Express’s functionality to configure the web server
+const app = express();
