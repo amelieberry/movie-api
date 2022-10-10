d2e439950ca817420ac1b7ac695b5ad696997411
amelieberry <aac.berry@gmail.com>
2022-10-10 13:48:45 +0200

    added GET requests

--- index.js
+++ index.js
@@ -2,3 +2,19 @@
 const express = require('express');
 // declares a variable that encapsulates Express’s functionality to configure the web server
 const app = express();
+
+let movies = [];
+
+// GET requests
+app.get('/', (req, res) => {
+    res.send('test text');
+});
+
+app.get('/movies', (req, res) => {
+    res.json(movies);
+});
+
+// listen for requests
+app.listen((8080, '0.0.0.0'), () => {
+    console.log('Your app is listening on port 8080.');
+});
