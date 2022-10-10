8b7c14fe872e8991f2ce97045afa5347fea33176
amelieberry <aac.berry@gmail.com>
2022-10-10 14:12:53 +0200

    added error handler

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 // Append Morgan logs to log.txt
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
+//set up the logger
 app.use(morgan('combined', {stream: accessLogStream}));
 
 let movies = [];
@@ -21,10 +22,14 @@ app.get('/movies', (req, res) => {
     res.json(movies);
 });
 
+// Serve the documentation.html file from the public folder
 app.use(express.static('public'));
 
 // handle errors
-app.use();
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Woops! Something broke.');
+});
 
 // listen for requests
 app.listen((8080, '0.0.0.0'), () => {
