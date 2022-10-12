87c4f5bd5e72dafe6b9ff36688af160c901bc8b5
amelieberry <aac.berry@gmail.com>
2022-10-12 13:39:03 +0200

    added delete favorite movie and user

--- index.js
+++ index.js
@@ -98,6 +98,20 @@ app.post('/users', (req, res) => {
     }
 });
 
+// Allow users to add a movie to their list of favorites
+app.post('/users/:id/:movieTitle', (req, res) => {
+    const { id, movieTitle } = req.params;
+
+    let user = users.find(user => user.id == id);
+
+    if (user) {
+        user.favoriteMovies.push(movieTitle);
+        res.status(200).send(`${movieTitle} has been added to user ${id}'s array of favorite movies`);
+    } else {
+        res.status(400).send('No such user');
+    } 
+});
+
 // READ
 // GET requests
 app.get('/', (req, res) => {
@@ -159,6 +173,35 @@ app.put('/users/:id', (req, res) => {
     } else {
         res.status(400).send('No such user')
     }
+});
+
+// DELETE
+// Allow users to remove a movie from their list of favorites
+app.delete('/users/:id/:movieTitle', (req, res) => {
+    const { id, movieTitle } = req.params;
+
+    let user = users.find(user => user.id == id);
+
+    if (user) {
+        user.favoriteMovies = user.favoriteMovies.filter(title => title !== movieTitle);
+        res.status(200).send(`${movieTitle} has been removed from user ${id}'s array of favorite movies`);
+    } else {
+        res.status(400).send('No such user');
+    } 
+});
+
+// Allow existing users to deregister 
+app.delete('/users/:id/', (req, res) => {
+    const { id } = req.params;
+
+    let user = users.find(user => user.id == id);
+
+    if (user) {
+        users =  users.filter(user => user.id != id);
+        res.status(200).send(`user ${id} has been deleted`);
+    } else {
+        res.status(400).send('No such user');
+    } 
 });
 
 // Serve the documentation.html file from the public folder
