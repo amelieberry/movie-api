c4f1e7beadd075d76a5be3149735b99112f11389
amelieberry <aac.berry@gmail.com>
2022-10-18 14:27:24 +0200

    UPDATE user info

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post('/users', (req, res) => {
 });
 
 // Allow users to add a movie to their list of favorites
-app.post('/users/:id/:movieTitle', (req, res) => {
+app.post('/users/:Username/:movies/:MovieID', (req, res) => {
     const { id, movieTitle } = req.params;
 
     let user = users.find(user => user.id == id);
@@ -140,19 +140,26 @@ app.get('/movies/directors/:directorName', (req, res) => {
 });
 
 // UPDATE
-// Allow users to update their user info
-app.put('/users/:id', (req, res) => {
-    const { id } = req.params;
-    const updatedUser = req.body;
-
-    let user = users.find(user => user.id == id);
-
-    if (user) {
-        user.username = updatedUser.username;
-        res.status(200).json(user);
-    } else {
-        res.status(400).send('No such user')
-    }
+// Update user info, by Username
+app.put('/users/:Username', (req, res) => {
+    Users.findOneAndUpdate({Username: req.params.Username}, 
+        {$set:
+            {
+                Username: req.body.Username,
+                Password: req.body.Password,
+                Email: req.body.Email,
+                Birthday: req.body.Birthday
+            }
+    },
+    {new: true}, // makes sure that the updated document is returned
+    (err, updatedUser) => {
+        if(err) {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        } else {
+            res.json(updatedUser);
+        }
+    });
 });
 
 // DELETE
