aecc453003132626b032ce657176a1b720813537
amelieberry <aac.berry@gmail.com>
2022-10-19 13:55:51 +0200

    Configured Passport strategies

---
+++ passport.js
@@ -0,0 +1,43 @@
+const passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy, // defines basic HTTP authentication for login requests
+    Models = require('./models.js'),
+    passportJWT = require('passport-jwt');
+
+let Users = Models.User,
+JWTStrategy = passportJWT.Strategy,
+ExtractJWT = passportJWT.ExtractJwt;
+
+// LocalStrategy taskes a username and password from request body. Uses Mongoose to check the database for a user with the same username
+passport.use(new LocalStrategy({
+    usernameField: 'Username',
+    passwordField: 'Password'
+}, (username, password, callback) => {
+    console.log(username + ' ' + password);
+    Users.findOne({Username: username}, (error, user) => {
+        if (error) {
+            console.log(error);
+            return callback(error);
+        }
+
+        if (!user) {
+            console.log('incorrect username');
+            return callback(null, false, {message: 'Incorrect username or password.'});
+        }
+
+        console.log('finished');
+        return callback(null, user);
+    });
+}));
+// allows authentication of users based on the JWT submitted alongside their request.
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'your_jwt_secret'
+}, (jwtPayLoad, callback) => {
+    return Users.findById(jwtPayload._id)
+    .then((user) => {
+        return callback(null, user);
+    })
+    .catch((erro) => {
+        return callback(error)
+    });
+}));
