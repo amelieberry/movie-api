b7f87dd4f2517a847a93c7a26bc33c63588ca28a
amelieberry <aac.berry@gmail.com>
2022-10-20 11:28:30 +0200

    install and use CORS

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ app.use(bodyParser.json());
 const Movies = Models.Movie;
 const Users = Models.User;
 
+const cors = require('cors');
+app.use(cors());
+
+// Allow access only to specific origins
+//let allowedOrigins = ['http://localhost:8080', 'the domain of the app goes here'];
+// app.use(cors({
+//     origin: (origin, callback) => {
+//         if(!origin) return callback(null, true);
+//         if(allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//             let message = 'The CORS policy for this application doesn’t allow access from origin' + origin;
+//             return callback(new Error(message), false);
+//         }
+//         return callback(null, true);
+//     }
+// }));
+
 let auth = require('./auth')(app);
 
 const passport = require('passport');
