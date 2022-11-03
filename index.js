36bda4b18802cff5d7a55622d1a19f3414e2962c
amelieberry <aac.berry@gmail.com>
2022-11-03 13:52:14 +0100

    re-inserted auth into movies endpoint

--- index.js
+++ index.js
@@ -127,9 +127,8 @@ app.get('/users/:Username', passport.authenticate('jwt', {session: false}), (req
     });
 });
 
-// ADD IT BACK: passport.authenticate('jwt', {session: false}), 
 // GET a list of all movies
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', {session: false}), (req, res) => {
     Movies.find()
     .then((movies) => {
         res.status(201).json(movies);
