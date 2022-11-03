a3dfdba3c5d663a5e4c2c6e1afe52906ff0b6de8
amelieberry <aac.berry@gmail.com>
2022-11-03 12:52:02 +0100

    moved CORS back to initial line

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const Models = require('./models.js');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const cors = require('cors');
-app.use(cors());
-
 const Movies = Models.Movie;
 const Users = Models.User;
+
+const cors = require('cors');
+app.use(cors());
 
 let auth = require('./auth')(app);
 
