8f5e28fe3de38b9130ee479b8788a5d8d826a33c
Amelie <aac.berry@gmail.com>
2022-12-23 13:11:41 +0100

    test movies not working

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ const cors = require("cors");
 // Allow access only to specific origins
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://trackm-client.netlify.app', 'https://main--trackm-client.netlify.app'];
 app.use(cors({
-    origin: (origin, callback) => {
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-            let message = 'The CORS policy for this application doesn’t allow access from origin' + origin;
-            return callback(new Error(message), false);
-        }
-        return callback(null, true);
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin' + origin;
+      return callback(new Error(message), false);
     }
+    return callback(null, true);
+  }
 }));
 
 const Movies = Models.Movie;
@@ -40,6 +40,7 @@ require("./passport");
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  dbName: "trackmDB"
 });
 
 // Append Morgan logs to log.txt
